460d8d67efa6c5034a932c89be4b86be4eb52669
Jonathan Haylett <jonathan.haylett@student.manchester.ac.uk>
2018-02-10 12:58:36 +0000

    Make script create .bash_aliases if it doesn't exist

--- setupLinks.sh
+++ setupLinks.sh
@@ -7,4 +7,4 @@ ln -sf .dotfiles/vim/vimrc $HOME/.vimrc
 ln -sf .dotfiles/inputrc $HOME/.inputrc
 ln -sf .dotfiles/tmux.conf $HOME/.tmux.conf
 ln -sf .dotfiles/bash_config $HOME/.bash_config
-ln -sf .dotfiles/bash_aliases $HOME/.bash_aliases
+touch $HOME/.bash_aliases
