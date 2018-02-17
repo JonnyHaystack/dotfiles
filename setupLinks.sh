3137f4a153ff4335645c3d768d92f041f4263b2e
Jonathan Haylett <jonathan.s.haylett@gmail.com>
2018-02-17 18:19:00 +0000

    Moved tmux.conf into tmux folder

--- setupLinks.sh
+++ setupLinks.sh
@@ -6,6 +6,6 @@ ln -sfn $HOME/.dotfiles/vim $HOME/.vim
 ln -sf $HOME/.dotfiles/vim/vimrc $HOME/.vimrc
 ln -sf $HOME/.dotfiles/inputrc $HOME/.inputrc
 ln -sfn $HOME/.dotfiles/tmux $HOME/.tmux
-ln -sf $HOME/.dotfiles/tmux.conf $HOME/.tmux.conf
+ln -sf $HOME/.dotfiles/tmux/tmux.conf $HOME/.tmux.conf
 ln -sf $HOME/.dotfiles/bash_config $HOME/.bash_config
 touch $HOME/.bash_aliases
