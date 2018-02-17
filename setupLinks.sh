ff7753055ab6d1c2f2c693b20a2e9204e5f28be9
Jonathan Haylett <jonathan.s.haylett@gmail.com>
2018-02-17 18:11:36 +0000

    Added Tmux Plugin Manager and Tmux Resurrect. Also fixed problem with symlinks to directories being created inside the directory if it already existed.

--- setupLinks.sh
+++ setupLinks.sh
@@ -2,10 +2,10 @@
 # Create symlinks for dotfiles so that they will appear in their
 # normal locations.
 
-ln -sf .dotfiles/vim $HOME/.vim
-ln -sf .dotfiles/vim/vimrc $HOME/.vimrc
-ln -sf .dotfiles/inputrc $HOME/.inputrc
-ln -sf .dotfiles/tmux $HOME/.tmux
-ln -sf .dotfiles/tmux.conf $HOME/.tmux.conf
-ln -sf .dotfiles/bash_config $HOME/.bash_config
+ln -sfn $HOME/.dotfiles/vim $HOME/.vim
+ln -sf $HOME/.dotfiles/vim/vimrc $HOME/.vimrc
+ln -sf $HOME/.dotfiles/inputrc $HOME/.inputrc
+ln -sfn $HOME/.dotfiles/tmux $HOME/.tmux
+ln -sf $HOME/.dotfiles/tmux.conf $HOME/.tmux.conf
+ln -sf $HOME/.dotfiles/bash_config $HOME/.bash_config
 touch $HOME/.bash_aliases
