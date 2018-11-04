b1956d20f78946c30cd592d4d63a08675b7a0994
Jonathan Haylett <jonathan.s.haylett@gmail.com>
2018-11-04 18:50:40 +0000

    Added i3 config

--- setupLinks.sh
+++ setupLinks.sh
@@ -2,13 +2,25 @@
 # Create symlinks for dotfiles so that they will appear in their
 # normal locations.
 
+# vim
 ln -sfn $HOME/.dotfiles/vim $HOME/.vim
 ln -sf $HOME/.dotfiles/vim/vimrc $HOME/.vimrc
 ln -sf $HOME/.dotfiles/vrapper/vrapperrc $HOME/.vrapperrc
 ln -sf $HOME/.dotfiles/ideavim/ideavimrc $HOME/.ideavimrc
+
+# .inputrc
 ln -sf $HOME/.dotfiles/inputrc $HOME/.inputrc
+
+# tmux
 ln -sfn $HOME/.dotfiles/tmux $HOME/.tmux
 ln -sf $HOME/.dotfiles/tmux/tmux.conf $HOME/.tmux.conf
+
+# bash
 ln -sf $HOME/.dotfiles/bash_config $HOME/.bash_config
-ln -sf $HOME/.dotfiles/gitconfig $HOME/.gitconfig
 touch $HOME/.bash_aliases
+
+# git
+ln -sf $HOME/.dotfiles/gitconfig $HOME/.gitconfig
+
+# i3
+ln -sfn $HOME/.dotfiles/i3 $HOME/.config/i3
