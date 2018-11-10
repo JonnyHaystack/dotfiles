500d3349617aa621d03b229da170b99648cee5d9
Jonathan Haylett <jonathan.s.haylett@gmail.com>
2018-11-10 22:44:36 +0000

    Updated requirements.txt for load workspace scripts

--- setupLinks.sh
+++ setupLinks.sh
@@ -4,7 +4,7 @@
 
 # scripts
 mkdir -p $HOME/scripts/
-ln -sfn $HOME/.dotfiles/scripts/i3/ $HOME/scripts/i3
+ln -sfn $HOME/.dotfiles/scripts/i3 $HOME/scripts/i3
 
 # desktop shortcuts
 mkdir -p $HOME/.local/share/applications/
@@ -18,8 +18,7 @@ ln -sf $HOME/.dotfiles/vrapper/vrapperrc $HOME/.vrapperrc
 ln -sf $HOME/.dotfiles/ideavim/ideavimrc $HOME/.ideavimrc
 
 # neovim
-mkdir -p $HOME/.config/nvim/
-ln -sf $HOME/.dotfiles/nvim/init.vim $HOME/.config/nvim/init.vim
+ln -sfn $HOME/.dotfiles/nvim $HOME/.config/nvim
 
 # .inputrc
 ln -sf $HOME/.dotfiles/inputrc $HOME/.inputrc
