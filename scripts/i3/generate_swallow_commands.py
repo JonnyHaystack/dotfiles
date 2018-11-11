9591c9e14370e64550df07682cab628718102ec0
Jonathan Haylett <jonathan.s.haylett@gmail.com>
2018-11-11 08:12:27 +0000

    Bug fixes and improvements to save workspace script

--- scripts/i3/generate_swallow_commands.py
+++ scripts/i3/generate_swallow_commands.py
@@ -10,11 +10,15 @@ def eprint(*args, **kwargs):
 
 # Command mappings for specific programs whose process command is not the same
 # as the one that should be used to launch it.
-# Format: 'class': ('command', terminal)
+# Format: 'class': 'command'
 window_class_command_mappings = {
-        'Gnome-terminal': ('gnome-terminal --working-directory=', True),
-        'Alacritty': ('alacritty --working-directory ', True),
+        'Gnome-terminal': 'gnome-terminal',
+        'Alacritty': 'alacritty',
         }
+
+# Specify which window classes are terminals so that we know to extract the
+# working directory from the window title.
+terminals = ['Gnome-terminal', 'Alacritty']
 
 i3 = i3ipc.Connection()
 
@@ -40,38 +44,31 @@ for con in i3.get_tree():
         # Get process info for the window.
         procinfo = psutil.Process(pid)
 
+        working_directory = procinfo.cwd()
+
+        # If the program is a terminal.
+        if con.window_class in terminals:
+            # Get the working directory from the window title (I'm sorry).
+            # Also remove any non ascii characters.
+            working_directory = con.name.encode('ascii',
+                    'ignore').split(': ')[1]
+            # Change ~ to $HOME.
+            working_directory = working_directory.replace('~', '$HOME')
+
         # Create command to launch program.
         # If there is a special command mapping for this program.
         if con.window_class in window_class_command_mappings:
-            # If the program is a terminal.
-            if window_class_command_mappings[con.window_class][1]:
-                # Get the working directory from the window title (I'm sorry)
-                # and stick it on the end of the command mapping.
-                # Also remove any non ascii characters.
-                working_directory = con.name.encode('ascii',
-                        'ignore').split(': ')[1]
-                # Change ~ to $HOME because gnome-terminal doesn't seem to
-                # support ~.
-                working_directory = working_directory.replace('~', '$HOME')
-                command = '{0}{1}'.format(
-                        window_class_command_mappings[con.window_class][0],
-                        working_directory,
-                        )
-            else:
-                # If the program is not a terminal, launch it by cd'ing to its
-                # working directory (obtained by psutil) and then executing
-                # its mapped command.
-                command = 'cd {0}; {1}&'.format(
-                        procinfo.cwd(),
-                        window_class_command_mappings[con.window_class][0],
-                        )
+            command = '(cd {0}; {1} &)'.format(
+                    working_directory,
+                    window_class_command_mappings[con.window_class],
+                    )
         else:
             # If the program has no special mapping, launch it by cd'ing to its
             # working directory (obtained by psutil) and then executing the
             # first index of the cmdline (hopefully this will work for almost
             # all programs I use at least).
-            command = 'cd {0}; {1}&'.format(
-                    procinfo.cwd(),
+            command = '(cd {0}; {1} &)'.format(
+                    working_directory,
                     procinfo.cmdline()[0],
                     )
         # Print the command.
