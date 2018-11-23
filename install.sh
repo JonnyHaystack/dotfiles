0b7a1fc64da889d311bb3a8414519927cc32aa8d
Jonathan Haylett <jonathan.s.haylett@gmail.com>
2018-11-23 18:15:11 +0000

    Added script to make URLs open faster in qutebrowser

--- install.sh
+++ install.sh
@@ -8,6 +8,7 @@ mkdir -p $HOME/bin/
 ln -sfn $HOME/.dotfiles/scripts/i3 $HOME/scripts/i3
 ln -sf $HOME/.dotfiles/scripts/dmenu-custom.sh $HOME/bin/dmenu-custom
 ln -sf $HOME/.dotfiles/scripts/dmenu-lpass.sh $HOME/bin/dmenu-lpass
+ln -sf $HOME/.dotfiles/scripts/open_url_in_instance.sh $HOME/bin/
 
 # desktop shortcuts
 mkdir -p $HOME/.local/share/applications/
