05eef2028ddeb0eefc78da6d320bb1e199bb50ec
Jonathan Haylett <jonathan.s.haylett@gmail.com>
2018-12-08 22:31:01 +0000

    Alter dmenu lastpass script to strip trailing/leading whitespace from entries

--- scripts/dmenu-lpass.sh
+++ scripts/dmenu-lpass.sh
@@ -3,6 +3,11 @@
 
 selection=$(echo -e $(lpass ls | awk -F '[/[]' '{print $(NF-1)"\\n"}') | ~/bin/dmenu-custom -l 10)
 
+shopt -s extglob
+selection="${selection##*( )}"
+selection="${selection%%*( )}"
+shopt -u extglob
+
 if [ "$selection" != "" ]; then
-    lpass show -c --password $selection &
+    lpass show -c --password "${selection}" &
 fi
