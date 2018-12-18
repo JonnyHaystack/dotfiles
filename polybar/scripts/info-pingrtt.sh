1c7d0f8e62a72cbdb4cb31c6bd61f4ad02a6ed33
Jonathan Haylett <jonathan.s.haylett@gmail.com>
2018-12-18 07:21:39 +0000

    Update polybar config

--- polybar/scripts/info-pingrtt.sh
+++ polybar/scripts/info-pingrtt.sh
@@ -8,11 +8,11 @@ else
     rtt=$(echo "$ping" | sed -rn 's/.*time=([0-9]{1,})\.?[0-9]{0,} ms.*/\1/p')
 
     if [ "$rtt" -lt 50 ]; then
-        text="%{F#3cb703}%{F-}"
+        text="%{u#38d638}%{F#666}%{F-}"
     elif [ "$rtt" -lt 150 ]; then
-        text="%{F#f9dd04}%{F-}"
+        text="%{u#e6d937}%{F#666}%{F-}"
     else
-        text="%{F#d60606}%{F-}"
+        text="%{u#f44336}%{F#666}%{F-}"
     fi
 
     echo $text $rtt ms
