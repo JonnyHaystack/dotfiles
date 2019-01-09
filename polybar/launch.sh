4390a37579035165c049d120d06f14708fe9b2e4
Jonathan Haylett <jonathan.s.haylett@gmail.com>
2019-01-09 22:51:04 +0000

    Update polybar launch script to dynamically find monitor configuration

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -6,7 +6,21 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
+# Get monitor configuration
+MONITORS=$(xrandr | grep " connected " | awk '{ print$1 }' | paste -d\; -s -)
+IFS=';' read -r -a MONITORS <<< "$MONITORS"
+export MONITORS
+export PRIMARY_MONITOR=$(xrandr | grep " primary " | awk '{ print$1 }' | tail -1)
+echo Primary monitor: $PRIMARY_MONITOR
+
 # Launch Polybar, using default config location ~/.config/polybar/config
-polybar bottom &
+export MONITOR=$PRIMARY_MONITOR && polybar bottom_primary &
+for i in "${MONITORS[@]}"
+do
+  if [ "$i" != "$PRIMARY_MONITOR" ]; then
+    echo External monitor: $i
+    export MONITOR=$i && polybar bottom_external &
+  fi
+done
 
 echo "Polybar launched..."
