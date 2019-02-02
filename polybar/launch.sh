64f9f94eaf34d0c5076cd32173971910a4968a70
Jonathan Haylett <jonathan.s.haylett@gmail.com>
2019-02-02 12:11:50 +0000

    Switch to hidden gnome-like system tray (polybar)

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -2,6 +2,7 @@
 
 # Terminate already running bar instances
 killall -q polybar
+ps -ef | grep 'hideIt.sh' | grep -v grep | awk '{print $2}' | xargs -r kill -9
 
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
@@ -10,17 +11,21 @@ while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 MONITORS=$(xrandr | grep " connected " | awk '{ print$1 }' | paste -d\; -s -)
 IFS=';' read -r -a MONITORS <<< "$MONITORS"
 export MONITORS
-export PRIMARY_MONITOR=$(xrandr | grep " primary " | awk '{ print$1 }' | tail -1)
+PRIMARY_MONITOR=$(xrandr | grep " primary " | awk '{ print$1 }' | tail -1)
 echo Primary monitor: $PRIMARY_MONITOR
 
 # Launch Polybar, using default config location ~/.config/polybar/config
-export MONITOR=$PRIMARY_MONITOR && polybar bottom_primary &
+export MONITOR=$PRIMARY_MONITOR
+polybar systray &
 for i in "${MONITORS[@]}"
 do
-  if [ "$i" != "$PRIMARY_MONITOR" ]; then
-    echo External monitor: $i
-    export MONITOR=$i && polybar bottom_external &
-  fi
+  echo Monitor: $i
+  export MONITOR=$i
+  polybar bottom &
 done
 
 echo "Polybar launched..."
+
+# Use hideIt.sh script to hide taskbar unless hovered on.
+bottom_left=$(xrandr | grep -w primary | awk -F'[x+ ]' '{print $6"x"$5+$7-36}')
+exec hideIt.sh -w --name '^Polybar tray window$' --region "$bottom_left"+10+-40
