4823ca51a7eddeb8a14e5c2c1355f55f29a0a045
Jonathan Haylett <jonathan.s.haylett@gmail.com>
2019-02-02 12:08:57 +0000

    Fix formatting in i3 save workspace script

--- scripts/i3/save_workspace.sh
+++ scripts/i3/save_workspace.sh
@@ -3,7 +3,7 @@
 if [ "$#" -eq 1 ]
 then
   i3-save-tree --workspace $1 | tail -n +2 | fgrep -v '// splitv' | fgrep -v \
-    '// splith'| sed 's|//||g' | sed -E '/\"title\"\:|\"window_role\"\:/d' \
+    '// splith' | sed 's|//||g' | sed -E '/\"title\"\:|\"window_role\"\:/d' \
     | sed '/^ *\}/{H;x;s/,\n/\n/;b};x;/^ *}/d' > ~/.i3/workspace_$1.json
 
   # Call python script to generate script to fill workspace.
