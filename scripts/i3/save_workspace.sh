887b3d0981018658d82acc1dd49ef130183c8567
Jonathan Haylett <jonathan.s.haylett@gmail.com>
2019-03-22 00:10:22 +0000

    Fix bugs in i3 save workspace script

--- scripts/i3/save_workspace.sh
+++ scripts/i3/save_workspace.sh
@@ -2,8 +2,8 @@
 
 if [ "$#" -eq 1 ]
 then
-  i3-save-tree --workspace $1 | tail -n +2 | fgrep -v '// splitv' | fgrep -v \
-    '// splith' | sed 's|//||g' | sed -E '/\"title\"\:|\"window_role\"\:/d' \
+  i3-save-tree --workspace $1 | tail -n +2 | grep -iv '// [a-z]' \
+    | sed 's|//||g' | sed -E '/\"title\"\:|\"window_role\"\:/d' \
     | sed '/^ *\}/{H;x;s/,\n/\n/;b};x;/^ *}/d' > ~/.i3/workspace_$1.json
 
   # Call python script to generate script to fill workspace.
