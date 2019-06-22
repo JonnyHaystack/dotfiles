f58d1f112d423855b068e4c2389ddc55984ea59b
Jonathan Haylett <jonathan@haylett.dev>
2019-06-22 01:41:04 +0100

    Update pynvim install script

--- scripts/pynvim_install.sh
+++ scripts/pynvim_install.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-virtualenv -p /usr/bin/python2 ~/.virtualenvs/pynvim2
-virtualenv -p /usr/bin/python3 ~/.virtualenvs/pynvim3
+virtualenv --system-site-packages -p /usr/bin/python2 ~/.virtualenvs/pynvim2
+virtualenv --system-site-packages -p /usr/bin/python3 ~/.virtualenvs/pynvim3
 source ~/.virtualenvs/pynvim2/bin/activate
 pip install pynvim
 source ~/.virtualenvs/pynvim3/bin/activate
