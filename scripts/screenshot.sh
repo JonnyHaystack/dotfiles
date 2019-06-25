7c8689049e84e865571ecc00f83c354106ead09e
Jonathan Haylett <jonathan@haylett.dev>
2019-06-25 15:11:37 +0100

    Add termite to compton transparency rules

--- scripts/screenshot.sh
+++ scripts/screenshot.sh
@@ -3,4 +3,4 @@
 filename=~/Pictures/Screenshots/img-$(date +%Y-%m-%d)-$(ls \
   ~/Pictures/Screenshots/img* | wc -l).png
 
-maim --hidecursor -s $filename && notify-send -i screenie 'Screenshot' $filename
+scrot -s $filename && notify-send -i screenie 'Screenshot' $filename
