57d71f47ac1c6698b9602d92dfa5f65c7cf6ec1f
Jonathan Haylett <jonathan@haylett.dev>
2019-06-25 15:12:02 +0100

    Revert "Add termite to compton transparency rules"
    This reverts commit 7c8689049e84e865571ecc00f83c354106ead09e.

--- scripts/screenshot.sh
+++ scripts/screenshot.sh
@@ -3,4 +3,4 @@
 filename=~/Pictures/Screenshots/img-$(date +%Y-%m-%d)-$(ls \
   ~/Pictures/Screenshots/img* | wc -l).png
 
-scrot -s $filename && notify-send -i screenie 'Screenshot' $filename
+maim --hidecursor -s $filename && notify-send -i screenie 'Screenshot' $filename
