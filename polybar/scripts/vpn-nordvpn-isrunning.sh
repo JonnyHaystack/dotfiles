23bfe194a6b98f7beec84293d9e4bdd45247513e
Jonathan Haylett <jonathan@haylett.dev>
2020-04-20 15:46:17 +0100

    Improve nordvpn scripts a bit

--- polybar/scripts/vpn-nordvpn-isrunning.sh
+++ polybar/scripts/vpn-nordvpn-isrunning.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 nordvpn_status=$(nordvpn status | grep Status | head -n 1)
-if [[ $nordvpn_status == *"Discon"* ]]; then
-  echo "%{u#f44336}%{+u}%{F#666}%{F-}"
-else
+if [[ $nordvpn_status == *"Connected"* ]]; then
   echo "%{u#38d638}%{+u}%{F#666}%{F-}"
+else
+  echo "%{u#f44336}%{+u}%{F#666}%{F-}"
 fi
