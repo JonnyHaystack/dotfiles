543d9ac637a430e4187833e48f201e3e95ed86a0
Jonathan Haylett <jonathan@haylett.dev>
2021-02-08 16:49:42 +0000

    Always put workspace 1 on primary monitor

--- i3/conf.d/10-outputs.sh
+++ i3/conf.d/10-outputs.sh
@@ -6,8 +6,17 @@ IFS=';' read -r -a MONITORS <<< "$MONITORS"
 export MONITORS
 PRIMARY_MONITOR=$(xrandr | grep " primary " | awk '{ print$1 }' | tail -1)
 
+MONITORS_SORTED=()
+MONITORS_SORTED+=($PRIMARY_MONITOR)
+
+for monitor in "${MONITORS[@]}"; do
+  if [ "$monitor" != "$PRIMARY_MONITOR" ]; then
+    MONITORS_SORTED+=($monitor)
+  fi
+done
+
 for i in {1..20}
 do
-  monitor=${MONITORS[$((($i - 1) % ${#MONITORS[@]}))]}
+  monitor=${MONITORS_SORTED[$((($i - 1) % ${#MONITORS_SORTED[@]}))]}
   echo "workspace $i output $monitor"
 done
