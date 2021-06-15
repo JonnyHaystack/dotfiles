ff0f1db60e2b6deab57db6dc55912569b7c87e11
Jonathan Haylett <jonathan@haylett.dev>
2021-06-15 13:34:48 +0100

    Update qutebrowser adblock lists

--- qutebrowser/config.py
+++ qutebrowser/config.py
@@ -118,6 +118,9 @@ c.content.javascript.can_access_clipboard = False
 c.content.autoplay = False
 c.content.cookies.accept = "no-3rdparty"
 
+# Adblock lists
+c.content.blocking.adblock.lists = ['https://easylist.to/easylist/easylist.txt', 'https://easylist.to/easylist/easyprivacy.txt', 'https://easylist-downloads.adblockplus.org/easylistdutch.txt', 'https://easylist-downloads.adblockplus.org/abp-filters-anti-cv.txt', 'https://www.i-dont-care-about-cookies.eu/abp/', 'https://secure.fanboy.co.nz/fanboy-cookiemonster.txt']
+
 # Fonts
 font_biggest = "11pt monospace"
 font_big = "10pt monospace"
